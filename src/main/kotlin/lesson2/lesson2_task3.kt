cd93743e8739e61cb003cfeb9246b5e83cf21697
LocalTheHazelnutCat <loffipop@proton.me>
2023-11-16 18:31:42 +0300

    KS-2-3 Заменил переменную константой

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -1,15 +1,16 @@
+const val NUMBER_CONST = 60
+
 fun main() {
 
     val timeOutHour = 9 // Время убытия
     val timeOutMinutes = 39 // Время убытия
     val timeWayMinutes = 457 // Минут займет путь
-    val numberConst = 60
 
-    val timeWayHours = timeWayMinutes / numberConst
+    val timeWayHours = timeWayMinutes / NUMBER_CONST
 
     var timeInMinutes = timeWayMinutes + timeOutMinutes
-    val timeInHours = timeOutHour + (timeInMinutes / numberConst)
-    timeInMinutes = (timeOutMinutes + timeWayMinutes) - ((timeInMinutes / numberConst) * numberConst)
+    val timeInHours = timeOutHour + (timeInMinutes / NUMBER_CONST)
+    timeInMinutes = (timeOutMinutes + timeWayMinutes) - ((timeInMinutes / NUMBER_CONST) * NUMBER_CONST)
 
     println("$timeInHours:$timeInMinutes")
 
