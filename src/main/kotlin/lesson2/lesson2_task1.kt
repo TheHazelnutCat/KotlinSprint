2e0924df59b645093c1055f21b297ecc1dc21e6d
LocalTheHazelnutCat <loffipop@proton.me>
2023-11-16 15:42:09 +0300

    KS-2-1 Обьявил переменные и сделал вывод среднего балла

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    val numberStudents: Byte = 4
+    val numberSumScore: Float = (3 + 4 + 3 + 5).toFloat()
+    val numberMiddleScore: Float = numberSumScore / numberStudents
+
+    println(numberMiddleScore)
+
+
+}
