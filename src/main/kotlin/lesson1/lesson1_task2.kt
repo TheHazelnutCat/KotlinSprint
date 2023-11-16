a9271a0e43fe0083b837dc2c6458d875386306eb
LocalTheHazelnutCat <loffipop@proton.me>
2023-11-16 14:31:27 +0300

    KS-1-2 Автоформатирование кода

--- src/main/kotlin/lesson1/lesson1_task2.kt
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -1,8 +1,8 @@
 fun main() {
 
-    var amountOrders : UByte = 75u // Тип Unsigned , т.к число заказов положительно или 0
-    var amountStaff : UShort
-    val textPurchase : String = "Спасибо за покупку!"
+    var amountOrders: UByte = 75u // Тип Unsigned , т.к число заказов положительно или 0
+    var amountStaff: UShort
+    val textPurchase: String = "Спасибо за покупку!"
 
     println(amountOrders)
     println(textPurchase)
