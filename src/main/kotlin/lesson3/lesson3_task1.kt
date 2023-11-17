b483b8a894b30dcb5be9ecad5f490203864a195e
LocalTheHazelnutCat <loffipop@proton.me>
2023-11-17 12:31:37 +0300

    KS-3-1 Обьявил переменные, сделал выводы приветствий

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val userName = "Иво"
+    val msgGreetingsDay = "Добрый день"
+    val msgGreetingsNight = "Добрый вечер"
+    var msgOutput: String
+
+    msgOutput = msgGreetingsDay
+    println("$msgOutput, $userName!")
+    msgOutput = msgGreetingsNight
+    println("$msgOutput, $userName!")
+
+}
