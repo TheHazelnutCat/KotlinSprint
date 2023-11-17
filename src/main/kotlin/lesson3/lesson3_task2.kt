245b0064294d78afb544330dd1d349652bc8d1e5
LocalTheHazelnutCat <loffipop@proton.me>
2023-11-17 12:41:40 +0300

    KS-3-2 Обьявил и перезаписал переменные, сделал выводы данных

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    val personName = "Татьяна"
+    var personSurname = "Андреева"
+    val personMiddleName = "Сергеевна"
+    var personAge = "20"
+
+    println("$personSurname $personName $personMiddleName, $personAge")
+
+    personSurname = "Сидорова"
+    personAge = "22"
+
+    println("$personSurname $personName $personMiddleName, $personAge")
+
+}
