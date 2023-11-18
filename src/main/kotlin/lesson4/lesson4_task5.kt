22b8602f5dc7d2a7c6ef5655fdb686cc1cbb6fe8
LocalTheHazelnutCat <loffipop@proton.me>
2023-11-18 21:39:16 +0300

    KS-4-5 Изменил переменные отборочных данных на константы

--- src/main/kotlin/lesson4/lesson4_task5.kt
+++ src/main/kotlin/lesson4/lesson4_task5.kt
@@ -1,14 +1,14 @@
 import java.util.Scanner
 
+const val LONG_COND_SHIP_INTACT = true // Альт: false
+const val LONG_COND_NUMBER_CREW_MIN = 55 //
+const val LONG_COND_NUMBER_CREW_MAX = 70 // Альт: Только 70
+const val LONG_COND_NUMBER_PROVISIONS = 51 // Альт: 51 и больше
+const val LONG_COND_GOOD_WEATHER = true // Альт: true
+
 fun main() {
 
-    val longCondShipIntact = true // Альт: Необязательно
-    val longCondNumberCrew = 55..70 // Альт: Только 70
-    val longCondNumberProvisions = 51 // Альт: 51 и больше
-    val longCondGoodWeather = true // Альт: обязательно
-
     val readerInput = Scanner(System.`in`)
-
     val actualShipIntact: Boolean = readerInput.nextBoolean()
     val actualNumberCrew: Int = readerInput.nextInt()
     val actualNumberProvisions: Int = readerInput.nextInt()
@@ -16,8 +16,8 @@ fun main() {
 
     println(
         "Возможность корабля отправиться в плавание: ${
-            ((longCondShipIntact == actualShipIntact) && (actualNumberCrew in longCondNumberCrew) && (longCondNumberProvisions <= actualNumberProvisions) && (longCondGoodWeather == actualGoodWeather))
-                    || ((longCondShipIntact != actualShipIntact) && (longCondNumberCrew.max() == actualNumberCrew) && (longCondNumberProvisions <= actualNumberProvisions) && (longCondGoodWeather == actualGoodWeather))
+            ((LONG_COND_SHIP_INTACT == actualShipIntact) && ((LONG_COND_NUMBER_CREW_MIN <= actualNumberCrew) and (LONG_COND_NUMBER_CREW_MAX >= actualNumberCrew)) && (LONG_COND_NUMBER_PROVISIONS <= actualNumberProvisions) && (LONG_COND_GOOD_WEATHER == actualGoodWeather))
+                    || ((LONG_COND_SHIP_INTACT != actualShipIntact) && (LONG_COND_NUMBER_CREW_MAX == actualNumberCrew) && (LONG_COND_NUMBER_PROVISIONS <= actualNumberProvisions) && (LONG_COND_GOOD_WEATHER == actualGoodWeather))
         }"
     )
 
