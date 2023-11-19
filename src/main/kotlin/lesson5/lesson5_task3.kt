1b89b504ee9fc96948edd6ffe874f67aab9283b0
LocalTheHazelnutCat <loffipop@proton.me>
2023-11-19 15:42:26 +0300

    KS-5-3 Иницилизировал переменную, добавил проверку условия, ввод и вывод

---
+++ src/main/kotlin/lesson5/lesson5_task3.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val rightAnswer = "20; 12"
+
+    println("Вам нужно угадать два числа от 0 до 42. Введите числа по порядку:")
+
+    val userAnswerFirst = readLine()!!
+    val userAnswerSecond = readLine()!!
+
+
+    if ((userAnswerFirst in rightAnswer) && (userAnswerSecond in rightAnswer))
+        println("Поздравляем! Вы выиграли главный приз!")
+    else if ((userAnswerFirst in rightAnswer) || (userAnswerSecond in rightAnswer))
+        println("Вы выиграли утешительный приз!")
+    else
+        println("Неудача!")
+}
