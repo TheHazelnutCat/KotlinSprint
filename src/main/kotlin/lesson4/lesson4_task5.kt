6bba797eee9a14b5d4e73fab9dc640e741e36693
LocalTheHazelnutCat <loffipop@proton.me>
2023-11-19 14:34:19 +0300

    KS-4-5 Изменил название всех переменных на более читаемые, исправил условия

--- src/main/kotlin/lesson4/lesson4_task5.kt
+++ src/main/kotlin/lesson4/lesson4_task5.kt
@@ -1,23 +1,23 @@
 import java.util.Scanner
 
-const val LONG_COND_SHIP_INTACT = true // Альт: false
-const val LONG_COND_NUMBER_CREW_MIN = 55 //
-const val LONG_COND_NUMBER_CREW_MAX = 70 // Альт: Только 70
-const val LONG_COND_NUMBER_PROVISIONS = 51 // Альт: 51 и больше
-const val LONG_COND_GOOD_WEATHER = true // Альт: true
+const val HAS_DAMAGE = true // Альт: false
+const val MIN_CREW_NUMBER = 55 //
+const val MAX_CREW_NUMBER = 70 // Альт: Только 70
+const val NUMBER_PROVISIONS = 50 // Альт: 51 и больше
+const val IS_GOOD_WEATHER = true // Альт: true
 
 fun main() {
 
     val readerInput = Scanner(System.`in`)
-    val actualShipIntact: Boolean = readerInput.nextBoolean()
-    val actualNumberCrew: Int = readerInput.nextInt()
-    val actualNumberProvisions: Int = readerInput.nextInt()
-    val actualGoodWeather: Boolean = readerInput.nextBoolean()
+    val hasNowDamage: Boolean = readerInput.nextBoolean()
+    val nowCrewNumber: Int = readerInput.nextInt()
+    val nowProvisionsNumber: Int = readerInput.nextInt()
+    val isNowGoodWeather: Boolean = readerInput.nextBoolean()
 
     println(
         "Возможность корабля отправиться в плавание: ${
-            ((LONG_COND_SHIP_INTACT == actualShipIntact) && ((LONG_COND_NUMBER_CREW_MIN <= actualNumberCrew) and (LONG_COND_NUMBER_CREW_MAX >= actualNumberCrew)) && (LONG_COND_NUMBER_PROVISIONS <= actualNumberProvisions) && (LONG_COND_GOOD_WEATHER == actualGoodWeather))
-                    || ((LONG_COND_SHIP_INTACT != actualShipIntact) && (LONG_COND_NUMBER_CREW_MAX == actualNumberCrew) && (LONG_COND_NUMBER_PROVISIONS <= actualNumberProvisions) && (LONG_COND_GOOD_WEATHER == actualGoodWeather))
+            ((HAS_DAMAGE == hasNowDamage) && ((MIN_CREW_NUMBER <= nowCrewNumber) and (MAX_CREW_NUMBER >= nowCrewNumber)) && (NUMBER_PROVISIONS < nowProvisionsNumber))
+                    || ((MAX_CREW_NUMBER == nowCrewNumber) && (NUMBER_PROVISIONS <= nowProvisionsNumber) && (IS_GOOD_WEATHER == isNowGoodWeather))
         }"
     )
 
