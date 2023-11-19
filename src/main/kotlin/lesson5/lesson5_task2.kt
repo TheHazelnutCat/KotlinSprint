062e64854a365de491ca0a42cb66c3ef41c0b9ab
LocalTheHazelnutCat <loffipop@proton.me>
2023-11-19 19:41:22 +0300

    KS-5-2 Заменил readline на readln

--- src/main/kotlin/lesson5/lesson5_task2.kt
+++ src/main/kotlin/lesson5/lesson5_task2.kt
@@ -2,7 +2,7 @@ const val AGE_OF_MAJORITY = 18
 
 fun main() {
 
-    val userYear = readLine()!!.toShort()
+    val userYear = readln().toShort()
     val nowYear: Short = 2023
 
     if ((nowYear - userYear) >= AGE_OF_MAJORITY)
