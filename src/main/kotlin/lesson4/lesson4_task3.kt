35b5f47617cb67e0ab43d0bd3ccb8c4e6052ac56
LocalTheHazelnutCat <loffipop@proton.me>
2023-11-19 14:48:43 +0300

    KS-4-3 Переименовал переменные для читабельности кода

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,17 +1,17 @@
-const val COND_SUN_STATUS: Boolean = true
-const val COND_AWNING_STATUS: Boolean = true
-const val COND_HUMIDITY: Int = 20
-const val COND_UN_SEASON: String = "зима"
+const val IS_SUNNY_WEATHER: Boolean = true
+const val IS_AWNING_OPEN: Boolean = true
+const val HUMIDITY: Int = 20
+const val UN_SEASON: String = "зима"
 
 fun main() {
 
-    val nowSunStatus: Boolean = true
-    val nowAwningStatus: Boolean = true
+    val isNowSunnyWeather: Boolean = true
+    val isNowAwningOpen: Boolean = true
     val nowHumidity: Int = 20
     val nowSeason: String = "зима"
 
     println(
         "Благоприятные ли условия сейчас для роста бобовых? " +
-                "${(COND_SUN_STATUS == nowSunStatus) && (COND_AWNING_STATUS == nowAwningStatus) && (COND_HUMIDITY == nowHumidity) && (COND_UN_SEASON != nowSeason)}"
+                "${(IS_SUNNY_WEATHER == isNowSunnyWeather) && (IS_AWNING_OPEN == isNowAwningOpen) && (HUMIDITY == nowHumidity) && (UN_SEASON != nowSeason)}"
     )
 }
